e752d21d6f8f41d800436e05c20af1d265162d8a
Orxan Huseynov <115917125+OrxanHuseynov1@users.noreply.github.com>
2025-02-07 09:25:08 +0400

    All

---
+++ Course.WebUI/Controllers/BookController.cs
@@ -0,0 +1,19 @@
+﻿using Course.Application.Abstract;
+using Course.WebUI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Course.WebUI.Controllers;
+
+public class BookController(IBookService bookService) : Controller
+{
+    private readonly IBookService _bookService = bookService;
+    public IActionResult Index()
+    {
+        var model = new BookListViewModel
+        {
+            Books = _bookService.GetAll()
+        };
+
+        return View(model);
+    }
+}
---
+++ Course.WebUI/Models/BookListViewModel.cs
@@ -0,0 +1,8 @@
+﻿using Course.Domain.Entites;
+
+namespace Course.WebUI.Models;
+
+public class BookListViewModel
+{
+    public List<Book> Books { get; set; } = [];
+}
